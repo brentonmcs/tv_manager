28cb45491443573e8919ade75a662de73734f976
Brenton McSweyn <brenton.mcsweyn@gmail.com>
2018-08-08 15:52:08 +1000

    removing test

--- setupTest.sh
+++
@@ -1,5 +0,0 @@
-go build
-mv tvrename ../../bin
-
-rm -f ~/Movies/Tv\ Shows/Sharp\ Objects/Season\ 1/Sharp\ Objects\ S01E01.mkv
-cp ~/Movies/Tv\ Shows/Sharp.Objects.S01E01.WEB.H264-DEFLATE.mkv ~/Downloads/
